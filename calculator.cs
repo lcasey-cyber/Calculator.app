1f30b57d5ef8509345b5e542d434484baae41fcf
casey <l.casey@students.amikom.ac.id>
2026-04-08 16:42:08 +0700

    Menambahkan fungsi penambahan

---
+++ calculator.cs
@@ -0,0 +1,9 @@
+class calculator
+
+	static void main(string[] args)
+	{
+		int a = 10;
+		int b = 6;
+		Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
